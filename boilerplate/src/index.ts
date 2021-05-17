10d1d018843219adae0f6ffeec95a6d33614378b
Cenglesson <englesson@live.se>
2021-05-17 17:53:16 +0200

    added loading page and animations

--- boilerplate/src/index.ts
+++ boilerplate/src/index.ts
@@ -6,7 +6,21 @@ import { TimerSetUp } from "./modules/TimerSetUp";
 
 
 const timer = new Timer();
-timer.start({countdown: true, startValues: {minutes: 10}});
+timer.start({ countdown: true, startValues: { minutes: 10 } });
 timer.addEventListener("secondsUpdated", () => {
     document.getElementById('time')!.innerHTML = timer.getTimeValues().toString();
+});
+
+const loadingpageElem: HTMLElement = document.querySelector('#loading');
+const startpageElem: HTMLElement = document.querySelector('#setuptimer');
+const changeBtn: HTMLElement = document.querySelector('.changeBtn');
+const pageElem: HTMLElement = document.querySelector('#time-step');
+
+
+
+changeBtn.addEventListener('click', () => {
+    loadingpageElem.classList.remove('active');
+    startpageElem.classList.add('active');
+    pageElem.classList.add('active');
+
 });
