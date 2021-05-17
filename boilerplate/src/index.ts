d67d79e61d7b4f3079427e9f6f9f01dec31877f9
jeppeGustafsson <jesper.hansgustaf@gmail.com>
2021-05-17 14:56:04 +0200

    Added some boilerplate + html skelton

--- boilerplate/src/index.ts
+++ boilerplate/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world!");
+import { Timer } from "easytimer.js";
+import { AnalogTimer } from "./modules/AnalogTimer";
+import { VisualTimer } from "./modules/VisualTimer";
+import { DigitalTimer } from "./modules/DigitalTimer";
+import { TimerSetUp } from "./modules/TimerSetUp";
+
+
+const timer = new Timer();
+timer.start({countdown: true, startValues: {minutes: 10}});
+timer.addEventListener("secondsUpdated", () => {
+    document.getElementById('time')!.innerHTML = timer.getTimeValues().toString();
+});
---
+++ boilerplate/src/modules/AnalogTimer.ts
@@ -0,0 +1,3 @@
+export const AnalogTimer = (time: number) => {
+    
+}
