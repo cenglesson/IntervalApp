e209b5f3b66b4d1fcf52ed562e86a2c503bf0652
Cenglesson <englesson@live.se>
2021-05-18 22:35:59 +0200

    done

--- boilerplate/src/modules/TextTimer.ts
+++ boilerplate/src/modules/TextTimer.ts
@@ -1,28 +1,31 @@
+import { Timer } from "easytimer.js";
+const myDiv = document.getElementById('textTimerTime') as HTMLElement;
+
 export const TextTimer = (time: number, type: string) => {
 
-    let myDiv = document.querySelector('#textTimerTime ');
-    let timeShow = document.querySelector('#displayTime');
+    const timer: Timer = new Timer();
 
+    timer.start({ countdown: true, startValues: { minutes: time } });
+
+    timer.addEventListener('secondsUpdated', (): void => {
+        timer.start()
+        const obj = timer.getTimeValues();
+        const minuteText = getTimeText(obj.minutes);
+        const secondsText = getTimeText(obj.seconds);
+
+        myDiv.innerText = `${minuteText}minutes and ${secondsText}seconds left`
+    })
+
+    timer.on('targetAchieved', () => {
+        console.log('DONE')
+    })
 
     let oneToTwenty = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ',
         'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
     let tenth = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    if (timeShow.toString().length > 7) return myDiv.innerHTML = 'DO NOT WORK!';
-    console.log(timeShow);
-    //let num = ('0000000000' + timeShow).slice(-10).match(/^(\d{1})(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-    let num = ('0000000' + timeShow).slice(-7).match(/^(\d{1})(\d{1})(\d{2})(\d{1})(\d{2})$/);
-    console.log(num);
-    if (!num) return;
-
-    let outputText = num[1] != 0 ? (oneToTwenty[Number(num[1])] || `${tenth[num[1][0]]} ${oneToTwenty[num[1][1]]}`) + ' million ' : '';
-
-    outputText += num[2] != 0 ? (oneToTwenty[Number(num[2])] || `${tenth[num[2][0]]} ${oneToTwenty[num[2][1]]}`) + 'hundred ' : '';
-    outputText += num[3] != 0 ? (oneToTwenty[Number(num[3])] || `${tenth[num[3][0]]} ${oneToTwenty[num[3][1]]}`) + ' thousand ' : '';
-    outputText += num[4] != 0 ? (oneToTwenty[Number(num[4])] || `${tenth[num[4][0]]} ${oneToTwenty[num[4][1]]}`) + 'hundred ' : '';
-    outputText += num[5] != 0 ? (oneToTwenty[Number(num[5])] || `${tenth[num[5][0]]} ${oneToTwenty[num[5][1]]} `) : '';
-
-    myDiv.innerHTML = outputText;
-
-
+    const getTimeText = (time: number) => {
+        if (time < 20) return oneToTwenty[time];
+        return tenth[Math.floor(time / 10)] + oneToTwenty[time % 10]
+    }
 }
