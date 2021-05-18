7cd9d742b281565eca31f36686bfcc8a5f58f343
Markus <70133910+MarkusG94@users.noreply.github.com>
2021-05-18 15:16:13 +0200

    changed startvalues

--- boilerplate/src/modules/DigitalTimer.ts
+++ boilerplate/src/modules/DigitalTimer.ts
@@ -5,7 +5,7 @@ const digitalShow = document.getElementById('digitalTimerTime') as HTMLElement;
 
 export const DigitalTimer = (time: number, type: string) => {
     const timer: Timer = new Timer();
-    timer.start({countdown: true, startValues: {seconds: time}});
+    timer.start({countdown: true, startValues: {minutes: time}});
 
 timer.addEventListener('secondsUpdated', ():void => {
     timer.start()
