9a112a234561f67042e972223ab6ae56e1b9fa28
Cenglesson <englesson@live.se>
2021-05-18 20:16:04 +0200

    added code

--- boilerplate/src/modules/TextTimer.ts
+++ boilerplate/src/modules/TextTimer.ts
@@ -1,3 +1,28 @@
 export const TextTimer = (time: number, type: string) => {
-    
+
+    let myDiv = document.querySelector('#textTimerTime ');
+    let timeShow = document.querySelector('#displayTime');
+
+
+    let oneToTwenty = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ',
+        'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+    let tenth = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (timeShow.toString().length > 7) return myDiv.innerHTML = 'DO NOT WORK!';
+    console.log(timeShow);
+    //let num = ('0000000000' + timeShow).slice(-10).match(/^(\d{1})(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    let num = ('0000000' + timeShow).slice(-7).match(/^(\d{1})(\d{1})(\d{2})(\d{1})(\d{2})$/);
+    console.log(num);
+    if (!num) return;
+
+    let outputText = num[1] != 0 ? (oneToTwenty[Number(num[1])] || `${tenth[num[1][0]]} ${oneToTwenty[num[1][1]]}`) + ' million ' : '';
+
+    outputText += num[2] != 0 ? (oneToTwenty[Number(num[2])] || `${tenth[num[2][0]]} ${oneToTwenty[num[2][1]]}`) + 'hundred ' : '';
+    outputText += num[3] != 0 ? (oneToTwenty[Number(num[3])] || `${tenth[num[3][0]]} ${oneToTwenty[num[3][1]]}`) + ' thousand ' : '';
+    outputText += num[4] != 0 ? (oneToTwenty[Number(num[4])] || `${tenth[num[4][0]]} ${oneToTwenty[num[4][1]]}`) + 'hundred ' : '';
+    outputText += num[5] != 0 ? (oneToTwenty[Number(num[5])] || `${tenth[num[5][0]]} ${oneToTwenty[num[5][1]]} `) : '';
+
+    myDiv.innerHTML = outputText;
+
+
 }
