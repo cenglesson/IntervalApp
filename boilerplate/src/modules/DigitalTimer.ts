a0c0c2fc2e4659010bd05ae993906ad631dd977a
Markus <70133910+MarkusG94@users.noreply.github.com>
2021-05-18 13:03:32 +0200

    basic countdown works

--- boilerplate/src/modules/DigitalTimer.ts
+++ boilerplate/src/modules/DigitalTimer.ts
@@ -1,3 +1,20 @@
+import { Timer } from "easytimer.js";
+
+const digitalEl = document.getElementById('DigitalTimer') as HTMLElement;
+const digitalShow = document.getElementById('digitalTimerTime') as HTMLElement;
+
 export const DigitalTimer = (time: number, type: string) => {
+    const timer = new Timer();
+    timer.start({countdown: true, startValues: {minutes: time}});
+
+timer.addEventListener('secondsUpdated', ():void => {
+    timer.start({
+        precision: 'secondTenths'
+    })
+    const obj = timer.getTimeValues();
+    digitalShow.innerText = `${obj.minutes.toString()}:${obj.seconds.toString()}`
+    
+})
     
 }
+
