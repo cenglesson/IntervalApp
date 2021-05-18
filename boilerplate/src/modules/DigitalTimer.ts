1c7f2ff6372417dbbab2d88c950e4ed97e55e68d
Markus <70133910+MarkusG94@users.noreply.github.com>
2021-05-18 13:39:14 +0200

    precision removed, not needed

--- boilerplate/src/modules/DigitalTimer.ts
+++ boilerplate/src/modules/DigitalTimer.ts
@@ -5,15 +5,18 @@ const digitalShow = document.getElementById('digitalTimerTime') as HTMLElement;
 
 export const DigitalTimer = (time: number, type: string) => {
     const timer: Timer = new Timer();
-    timer.start({countdown: true, startValues: {minutes: time}});
+    timer.start({countdown: true, startValues: {seconds: time}});
 
 timer.addEventListener('secondsUpdated', ():void => {
-    timer.start({
-        precision: 'secondTenths'
-    })
+    timer.start()
     const obj: any = timer.getTimeValues();
     digitalShow.innerText = `${obj.minutes.toString()}:${obj.seconds.toString()}`
+
+   
  
+})
+timer.on('targetAchieved', () => {
+    console.log('WOHOOO')
 })
     
 }
